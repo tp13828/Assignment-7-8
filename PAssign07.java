26614c5b22cbb55b9ef67501a9b70ff3344db2dc
tp13828 <150047822+tp13828@users.noreply.github.com>
2023-11-17 11:39:28 -0500

    Add files via upload

--- PAssign07.java
+++ PAssign07.java
@@ -2,9 +2,10 @@
  * File Name: PAssign07.java
  * Class: CSCI 1302
  * Author: Stella Pham
- * Created on: Oct 26, 2023
- * Last Modified: Oct 27, 2023
+ * Created on: Nov 5, 2023
+ * Last Modified: Nov 17, 2023
  * Description: JavaFX GUI designed to look like a keypad    
+ * Hello-World GitHub Link: https://github.com/tp13828/hello-world.git
  */
 
 package keypad;
@@ -12,15 +13,41 @@ package keypad;
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import javafx.geometry.*;
 
 public class PAssign07 extends Application {
+	private TextField txtAnswer = new TextField();
+	private Label lblProblem = new Label("What are the first 7 digits of PI?");
+
 	public void start(Stage primaryStage) {
 		BorderPane borderPane = new BorderPane();
-		KeyPadPane keyPane = new KeyPadPane();
+		borderPane.setStyle("-fx-background-color:#fcffff");
+		KeyPadPane keyPane = new KeyPadCustomPane();
+		keyPane.setAlignment(Pos.CENTER);
 
-		Scene scene = new Scene(borderPane, 800, 800);
+		//Add/Change Answer TextField
+		txtAnswer.setAlignment(Pos.BOTTOM_CENTER);
+		txtAnswer.setPrefSize(800, 100);
+		txtAnswer.setEditable(false);
+
+		//Add/Change Problem Label
+		lblProblem.setFont(Font.font("Verdana", FontWeight.BOLD, 40));
+		lblProblem.setPrefSize(800, 100);
+		lblProblem.setAlignment(Pos.TOP_CENTER);
+
+		//Add elements to borderpane
+		borderPane.setCenter(keyPane);
+		borderPane.setTop(lblProblem);
+		borderPane.setBottom(txtAnswer);
+
+		//Set scene and show
+		Scene scene = new Scene(borderPane, 1000, 700);
 		primaryStage.setTitle("KeyPad");
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -32,8 +59,18 @@ public class PAssign07 extends Application {
 	}
 }
 
-class KeyPadUpgrade extends KeyPadPane {
-	public KeyPadUpgrade() {
-		
+//Custom Keypad Upgrade
+class KeyPadCustomPane extends KeyPadPane {
+	public KeyPadCustomPane() {
+		// Change Button Visuals
+		for (int i = 0; i < listButtons.size(); i++) {
+			listButtons.get(i).setPrefSize(120, 120);
+			listButtons.get(i).setStyle("-fx-background-color:#e3fcff; -fx-border-color: black;");
+			listButtons.get(i).setFont(Font.font("Verdana", FontWeight.BOLD, 50));
+		}
+
+		// Change Blank Buttons
+		btnBlank1.setText(".");
+		btnBlank2.setText("=");
 	}
 }
